7f4cf469abb8c85ad38b3532204fbd47c2a83268
Linhurdos <teremdev@gmail.com>
2019-05-21 19:37:59 +0300

    'refactoring/card-interval' now card calculate interval, deck only contain settings of gaps

---
+++ include/memedar/model/deck/gap.hpp
@@ -0,0 +1,58 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+
+ * Copyright (C) 2018 Roman Erdyakov (Linhurdos) <teremdev@gmail.com>
+
+ * This file is part of Memedar (flashcard system)
+ * Memedar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef MEMEDAR_MODEL_DECK_GAP_HPP
+#define MEMEDAR_MODEL_DECK_GAP_HPP
+
+
+namespace md::model::deck {
+	using gap_ratio = unsigned int;
+	class gap;
+	class gaps;
+}
+
+
+class md::model::deck::gap
+{
+public:
+	gap(std::time_t gap_value, md::model::deck::gap_ratio gap_ratio);
+
+	std::time_t value;
+	std::time_t ratio;
+};
+
+
+class md::model::deck::gaps
+{
+public:
+	gaps();
+	gaps(md::model::deck::gap good_gap,
+	     md::model::deck::gap easy_gap);
+
+	md::model::deck::gap good_gap() const;
+	md::model::deck::gap easy_gap() const;
+protected:
+	md::model::deck::gap m_good_gap;
+	md::model::deck::gap m_easy_gap;
+};
+
+
+#endif // MEMEDAR_MODEL_DECK_GAP_HPP
