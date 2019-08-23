ced0b334cbb9d3050505b0317cda103953ee5ade
Rephobia <26533256+Rephobia@users.noreply.github.com>
2019-08-23 19:25:53 +0300

    Add browser

--- src/presenter/controller.cpp
+++ src/presenter/controller.cpp
@@ -32,6 +32,7 @@
 #include "memedar/view/lobby.hpp"
 #include "memedar/view/designer.hpp"
 #include "memedar/view/lesson.hpp"
+#include "memedar/view/browser.hpp"
 
 #include "memedar/model/task/task.hpp"
 #include "memedar/model/task/taskbook.hpp"
@@ -40,6 +41,8 @@
 #include "memedar/presenter/lobby_presenter.hpp"
 #include "memedar/presenter/lesson_presenter.hpp"
 #include "memedar/presenter/designer_presenter.hpp"
+#include "memedar/presenter/browser_presenter.hpp"
+
 #include "memedar/presenter/controller.hpp"
 
 
@@ -52,18 +55,21 @@ controller::controller(md::model::service& service,
                        md::view::menu& menu,
                        md::view::lobby& lobby,
                        md::view::lesson& lesson,
-                       md::view::designer& designer)
+                       md::view::designer& designer,
+                       md::view::browser& browser)
 	: m_service            {service}
 	, m_error_delegate     {error_delegate}
 	, m_menu               {menu}
 	, m_lobby              {lobby}
 	, m_lesson             {lesson}
 	, m_designer           {designer}
+	, m_browser            {browser}
 	, m_presenter          {nullptr}
 	, m_designer_presenter {nullptr}
 {
 	m_menu.call_lobby.connect([this]() { run_lobby(); });
 	m_menu.call_designer.connect([this]() { add_deck(); });
+	m_menu.call_browser.connect([this]() { run_browser(); });
 	m_designer.cancel.connect([this]() { m_presenter->run(); });
 	run_lobby();
 }
@@ -90,6 +96,17 @@ void controller::run_lesson(md::model::deck::deck& deck)
 	}
 }
 
+void controller::run_browser()
+{
+	try {
+		m_presenter = std::make_unique<md::browser_presenter>
+			(*this, m_service, m_error_delegate, m_browser);
+	}
+	catch (std::system_error& e) {
+		m_error_delegate.show_error(e);
+	}	
+}
+
 void controller::add_card(md::model::deck::deck& deck)
 {
 	try {
@@ -109,7 +126,7 @@ void controller::update_task(md::model::deck::deck& deck, md::model::task::task&
 	}
 	catch (std::system_error& e) {
 		m_error_delegate.show_error(e);
-	}	
+	}
 }
 
 void controller::add_deck()
---
+++ src/view/qt/browser.cpp
@@ -0,0 +1,108 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+
+ * Copyright (C) 2019 Roman Erdyakov
+
+ * This file is part of Memedar (flashcard system)
+ * Memedar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#include <ctime>
+#include <memory>
+#include <deque>
+
+#include <boost/signals2.hpp>
+#include <QString>
+#include <QMainWindow>
+#include <QStandardItemModel>
+#include <QStandardItem>
+
+#include "memedar/utils/storage.hpp"
+
+#include "memedar/model/side/side.hpp"
+#include "memedar/model/card/card.hpp"
+#include "memedar/model/deck/deck.hpp"
+
+#include "memedar/view/browser.hpp"
+#include "memedar/view/qt/ui/button.hpp"
+#include "memedar/view/qt/ui/box.hpp"
+#include "memedar/view/qt/main_window.hpp"
+#include "memedar/view/qt/browser.hpp"
+
+
+using md::view::qt::browser;
+
+browser::table_row::table_row(md::model::deck::deck& new_deck,
+                              md::model::card::card& new_card)
+	: deck {new_deck}
+	, card {new_card}
+{ ;}
+
+browser::table::table(QWidget* parent)
+	: QTableView {parent}
+	, m_model    {new QStandardItemModel {}}
+{
+	QTableView::setSelectionBehavior(QAbstractItemView::SelectRows);
+	QTableView::setEditTriggers(QAbstractItemView::NoEditTriggers);
+	QTableView::setModel(m_model);
+}
+
+md::model::card::card& browser::table::get_card(std::size_t row_index)
+{
+	return m_rows[row_index].card;
+}
+
+void browser::table::add_row(md::model::deck::deck& deck, md::model::card::card& card)
+{
+	m_model->setItem(m_row, column::deck, new QStandardItem {deck.name()});
+	m_model->setItem(m_row, column::question, new QStandardItem {card.question.text()});
+	m_model->setItem(m_row, column::answer, new QStandardItem {card.answer.text()});
+	
+	m_rows.push_back(table_row {deck, card});
+	
+	m_row++;
+}
+
+
+browser::browser(qt::main_window* main_window)
+	: m_main_window {main_window}
+{ ;}
+
+void browser::show(std::deque<md::model::deck::deck>& decks)
+{
+	auto table {new browser::table {m_main_window}};
+	
+	for (auto& deck : decks) {
+		
+		for (auto& card : deck) {
+
+			table->add_row(deck, *card);
+
+		}
+	}
+	
+	QObject::connect(table,
+	                 &QAbstractItemView::doubleClicked,
+	                 [this, table](const QModelIndex& index)
+	                 {
+		                 decltype(auto) card {table->get_card(index.row())};
+
+		                 call_designer(card);
+	                 });
+		
+	m_main_window->set_widget(table);
+}
+
+
